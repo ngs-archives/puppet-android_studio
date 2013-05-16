ee7aad4754b17ed847ae3abadeceaebe7ff7bf76
Atsushi Naganse <a@ngs.io>
2013-05-16 01:14:45 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,17 @@
+# Android Studio Puppet Module for Boxen
+
+## Usage
+
+```puppet
+include andriod_studio
+```
+
+## Required Puppet Modules
+
+None.
+
+## Developing
+
+Write code.
+
+Run `script/cibuild`.
---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+class android_studio ( $version = '130.677228' ) {
+  package { 'Android Studio':
+    source   => 'http://dl.google.com/android/studio/android-studio-bundle-${version}-mac.dmg',
+    provider => 'appdmg'
+  }
+}
