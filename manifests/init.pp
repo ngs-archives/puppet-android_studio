d54e5c06073a598da8cb868bfd420f83042a622b
Atsushi Nagase <a@ngs.io>
2013-06-30 20:58:30 +0900

    Update default  #1

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class android_studio ( $version = '130.677228' ) {
+class android_studio ( $version = '130.687321' ) {
   package { 'Android Studio':
     source   => "http://dl.google.com/android/studio/android-studio-bundle-${version}-mac.dmg",
     provider => 'appdmg'
